3bf931044cb6995006f936f8ee359d1021f7c7a8
052hide <hidetoshi1990@gmail.com>
2020-03-15 00:18:32 +0900

    lintfix

--- functions/src/modules/scraping.ts
+++ functions/src/modules/scraping.ts
@@ -1,18 +1,23 @@
 import * as functions from 'firebase-functions'
 import * as puppeteer from 'puppeteer'
 
-module.exports = functions
-  .https.onRequest(async (_req, _res) => {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.goto('https://www.metro.tokyo.lg.jp/')
-    await page.waitForSelector('#tmp_top_news_cnt')
-    const elm = (await ((await ((await page.$$('#tmp_top_news_cnt .datatable tbody tr'))[0]).$$('td'))[1]).$$('p'))[0]
-    const text = (await (await elm.getProperty('textContent')).jsonValue()) as string
-    console.info(text)
+module.exports = functions.https.onRequest(async (_req, _res) => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://www.metro.tokyo.lg.jp/')
+  await page.waitForSelector('#tmp_top_news_cnt')
+  const elm = (
+    await (
+      await (await page.$$('#tmp_top_news_cnt .datatable tbody tr'))[0].$$('td')
+    )[1].$$('p')
+  )[0]
+  const text = (await (
+    await elm.getProperty('textContent')
+  ).jsonValue()) as string
+  console.info(text) // eslint-disable-line
 
-    await page.close()
-    await browser.close()
+  await page.close()
+  await browser.close()
 
-    _res.status(200).send('success scraping')
-  })
+  _res.status(200).send('success scraping')
+})
